b4006b0d9499fa6656c4ce1f8dae0225c68505c0
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2014-08-07 00:11:03 -0300

    Agrego función.
    Nueva función, force yes
    
    Gracias Ignacio Rodríguez por la sugerencia. may the force be with you.

--- xubucleaner.sh
+++ xubucleaner.sh
@@ -4,9 +4,9 @@
 OLD=$(dpkg -l|grep "^rc"|awk '{print $2}')
 A="\033[1;33m"
 R="\033[0;31m"
-F="\033[0m"
+FINAL="\033[0m"
 
-function chauoldkernel
+function borrar_oldkernel
 {
 	ls /boot/ | grep vmlinuz | sed 's@vmlinuz-@linux-image-@g' | sed '$d' | sed '$d' > /tmp/kernelList
 	if [ -s /tmp/kernelList ]; then
@@ -24,54 +24,54 @@ function chauoldkernel
 
 function limpiar
 {
-	echo $A"Limpiando la cache apt..."$F
+	echo -e $A"Limpiando la cache apt..."$FINAL
 	apt-get -y clean
 
-	echo $A"Eliminando paquetes huerfanos..."$F
+	echo -e $A"Eliminando paquetes huerfanos..."$FINAL
 	apt-get -y autoremove
 
-	echo $A"Removiendo viejos archivos de configuración..."$F
+	echo -e $A"Removiendo viejos archivos de configuración..."$FINAL
 	apt-get purge $OLD
 
-	echo $A"Removiendo viejos kernels..."$F
-	chauoldkernel
+	echo -e $A"Removiendo viejos kernels..."$FINAL
+	borrar_oldkernel
 
-	echo $A"Limpiando imágenes en miniatura..."$F
+	echo -e $A"Limpiando imágenes en miniatura..."$FINAL
 	rm -rf /home/*/.thumbnails/large/*
 	rm -rf /home/*/.thumbnails/normal/*
 
-	echo $A"Limpiando caché de Firefox..."$F
+	echo -e $A"Limpiando caché de Firefox..."$FINAL
 	rm -rf /home/*/.cache/mozilla/firefox/*
 
-	echo $A"Limpiando caché de Google Chrome..."$F
+	echo -e $A"Limpiando caché de Google Chrome..."$FINAL
 	rm -rf /home/*/.cache/google-chrome/*
 }
 
 if [ $USER != root ]; then
-  echo $R"Error: tenes que ser root"
-  echo $A"Saliendo..."$F
+  echo -e $R"Error: tenes que ser root"
+  echo -e $A"Saliendo..."$FINAL
   notify-send "Xubucleaner" "Tenés que ejecutar este programa como root"
   exit 0
 fi
 clear
 notify-send "Xubucleaner" "Iniciando limpieza..."
 
-echo $A"Limpiando las papeleras..."$F
+echo -e $A"Limpiando las papeleras..."$FINAL
 rm -rf /home/*/.local/share/Trash/*/**
 rm -rf /root/.local/share/Trash/*/**
 
 limpiar
 
-echo $A"Obteniendo información de los repositorios..."$FINAL
+echo -e $A"Obteniendo información de los repositorios..."$FINAL
 apt-get -y update
 
-echo $A"Actualizándo programas..."$FINAL
-apt-get -y upgrade
+echo -e $A"Actualizándo programas..."$FINAL
+apt-get -y --force-yes upgrade
 
-echo $A"Actualizándo kernel..."$FINAL
-apt-get -y dist-upgrade
+echo -e $A"Actualizándo kernel..."$FINAL
+apt-get -y --force-yes dist-upgrade
 
 limpiar
 
-echo $A"Script finalizado - edición por: Carlos Planchón!"$FINAL
+echo -e $A"Script finalizado - edición por: Carlos Planchón!"$FINAL
 notify-send "Xubucleaner" "Listo!"
