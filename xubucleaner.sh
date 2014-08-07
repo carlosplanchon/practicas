e1c93e002169758099917332cef46e86017fc2cc
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2014-08-07 15:43:15 -0300

    Actualización de Xubucleaner
    Agrego función de reparación de paquetes rotos.

--- xubucleaner.sh
+++ xubucleaner.sh
@@ -4,7 +4,7 @@
 OLD=$(dpkg -l|grep "^rc"|awk '{print $2}')
 A="\033[1;33m"
 R="\033[0;31m"
-FINAL="\033[0m"
+R="\033[0m"
 
 function borrar_oldkernel
 {
@@ -24,54 +24,62 @@ function borrar_oldkernel
 
 function limpiar
 {
-	echo -e $A"Limpiando la cache apt..."$FINAL
+	echo -e $A"Limpiando la cache apt..."$R
 	apt-get -y clean
 
-	echo -e $A"Eliminando paquetes huerfanos..."$FINAL
+	echo -e $A"Eliminando paquetes huerfanos..."$R
 	apt-get -y autoremove
 
-	echo -e $A"Removiendo viejos archivos de configuración..."$FINAL
+	echo -e $A"Eliminando paquetes viejos..."$R
+	apt-get -y autoclean
+
+	echo -e $A"Removiendo viejos archivos de configuración..."$R
 	apt-get -y --force-yes purge $OLD
 
-	echo -e $A"Removiendo viejos kernels..."$FINAL
+	echo -e $A"Removiendo viejos kernels..."$R
 	borrar_oldkernel
 
-	echo -e $A"Limpiando imágenes en miniatura..."$FINAL
+	echo -e $A"Limpiando imágenes en miniatura..."$R
 	rm -rf /home/*/.thumbnails/large/*
 	rm -rf /home/*/.thumbnails/normal/*
 
-	echo -e $A"Limpiando caché de Firefox..."$FINAL
+	echo -e $A"Limpiando caché de Firefox..."$R
 	rm -rf /home/*/.cache/mozilla/firefox/*
 
-	echo -e $A"Limpiando caché de Google Chrome..."$FINAL
+	echo -e $A"Limpiando caché de Google Chrome..."$R
 	rm -rf /home/*/.cache/google-chrome/*
 }
 
 if [ $USER != root ]; then
   echo -e $R"Error: tenes que ser root"
-  echo -e $A"Saliendo..."$FINAL
+  echo -e $A"Saliendo..."$R
   notify-send "Xubucleaner" "Tenés que ejecutar este programa como root"
   exit 0
 fi
 clear
 notify-send "Xubucleaner" "Iniciando limpieza..."
 
-echo -e $A"Limpiando las papeleras..."$FINAL
+echo -e $A"Limpiando las papeleras..."$R
 rm -rf /home/*/.local/share/Trash/*
 rm -rf /root/.local/share/Trash/*
 
 limpiar
 
-echo -e $A"Obteniendo información de los repositorios..."$FINAL
+echo -e $A"Obteniendo información de los repositorios..."$R
 apt-get -y update
 
-echo -e $A"Actualizándo programas..."$FINAL
+echo -e $A"Actualizándo programas..."$R
 apt-get -y --force-yes upgrade
 
-echo -e $A"Actualizándo kernel..."$FINAL
+echo -e $A"Actualizándo kernel..."$R
 apt-get -y --force-yes dist-upgrade
 
 limpiar
 
-echo -e $A"Script finalizado - edición por: Carlos Planchón!"$FINAL
+echo -e $A"Arreglando paquetes rotos (si los hay)..."$R
+apt-get -y --force-yes -f install
+dpkg --configure -a
+apt-get check
+
+echo -e $A"Script finalizado - edición por: Carlos Planchón!"$R
 notify-send "Xubucleaner" "Listo!"
