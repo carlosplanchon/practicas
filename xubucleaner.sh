7be5bf925e27c75e5ef8d50332e84cc8cddcc2f4
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2016-06-18 16:09:34 -0300

    Removed deprecated --force-yes
    Force yes has been replaced by: --allow-downgrades --allow-remove-essential --allow-change-held-packages

--- xubucleaner.sh
+++ xubucleaner.sh
@@ -7,47 +7,47 @@ F='\033[0m'
 
 function limpiar
 {
-	echo -e $A'Limpiando la cache apt...'$F
-	apt-get -y clean
+    echo -e $A'Limpiando la cache apt...'$F
+    apt-get -y clean
 
-	echo -e $A'Eliminando paquetes huerfanos...'$F
-	apt-get -y autoremove
+    echo -e $A'Eliminando paquetes huerfanos...'$F
+    apt-get -y autoremove
 
-	echo -e $A'Eliminando paquetes viejos...'$F
-	apt-get -y autoclean
+    echo -e $A'Eliminando paquetes viejos...'$F
+    apt-get -y autoclean
 
-	echo -e $A'Removiendo viejos archivos de configuración...'$F
-	apt-get -y --force-yes purge $(dpkg -l|grep '^rc'|awk '{print $2}')
+    echo -e $A'Removiendo viejos archivos de configuración...'$F
+    apt-get -y --allow-downgrades --allow-remove-essential --allow-change-held-packages purge $(dpkg -l|grep '^rc'|awk '{print $2}')
 
-	echo -e $A'Removiendo viejos kernels (si los hay)...'$F
-	ls /boot/ | grep vmlinuz | sed 's@vmlinuz-@linux-image-@g' | sed '$d' | sed '$d' > /tmp/kernelList
-	if [ -s /tmp/kernelList ]; then
-		echo -e $A'Se eliminarán los siguientes kernels\n`cat /tmp/kernelList`'$F
-		notify-send 'Xubucleaner' 'Se está operando sobre el kernel.'
-		for I in `cat /tmp/kernelList`; do
-			apt-get remove $I
-			echo -e $A'Eliminando $I...'$F
-		done
-		rm -f /tmp/kernelList
-		echo -e $A'Actualizando gestor de arranque...'$F
-		update-grub
-	fi
+    echo -e $A'Removiendo viejos kernels (si los hay)...'$F
+    ls /boot/ | grep vmlinuz | sed 's@vmlinuz-@linux-image-@g' | sed '$d' | sed '$d' > /tmp/kernelList
+    if [ -s /tmp/kernelList ]; then
+        echo -e $A'Se eliminarán los siguientes kernels\n`cat /tmp/kernelList`'$F
+        notify-send 'Xubucleaner' 'Se está operando sobre el kernel.'
+        for I in `cat /tmp/kernelList`; do
+            apt-get remove $I
+            echo -e $A'Eliminando $I...'$F
+        done
+        rm -f /tmp/kernelList
+        echo -e $A'Actualizando gestor de arranque...'$F
+        update-grub
+    fi
 
-	echo -e $A'Limpiando imágenes en miniatura...'$F
-	rm -rf /home/*/.thumbnails/large/*
-	rm -rf /home/*/.thumbnails/normal/*
+    echo -e $A'Limpiando imágenes en miniatura...'$F
+    rm -rf /home/*/.thumbnails/large/*
+    rm -rf /home/*/.thumbnails/normal/*
 
-	echo -e $A'Limpiando caché'$F
-	rm -rf /home/*/.cache/*
+    echo -e $A'Limpiando caché'$F
+    rm -rf /home/*/.cache/*
 
-	echo -e $A'Limpiando archivos temporales...'$F
-	rm -rf /tmp/*
-	rm -rf /var/tmp/*
+    echo -e $A'Limpiando archivos temporales...'$F
+    rm -rf /tmp/*
+    rm -rf /var/tmp/*
 
-	echo -e $A'Limpiando registros...(si existen en este sistema)'$F
-	rm /usr/bin/TEST.log
-	rm /usr/bin/RECV.log
-	rm /usr/bin/SENT.log
+    echo -e $A'Limpiando registros...(si existen en este sistema)'$F
+    rm /usr/bin/TEST.log
+    rm /usr/bin/RECV.log
+    rm /usr/bin/SENT.log
 }
 
 if [ $USER != root ]; then
@@ -66,7 +66,7 @@ rm -rf /root/.local/share/Trash/*
 limpiar
 
 echo -e $A'Arreglando paquetes rotos (si los hay)...'$F
-apt-get -y --force-yes -f install
+apt-get -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -f install
 dpkg --configure -a
 apt-get check
 
@@ -74,10 +74,10 @@ echo -e $A'Obteniendo información de los repositorios...'$F
 apt-get -y update
 
 echo -e $A'Actualizándo programas...'$F
-apt-get -y --force-yes upgrade
+apt-get -y --allow-downgrades --allow-remove-essential --allow-change-held-packages upgrade
 
 echo -e $A'Actualizándo kernel...'$F
-apt-get -y --force-yes dist-upgrade
+apt-get -y --allow-downgrades --allow-remove-essential --allow-change-held-packages dist-upgrade
 
 limpiar
 
