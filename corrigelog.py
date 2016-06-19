1081a1019048962e9bad58d9a22fa4e1ecca2821
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2016-06-18 22:53:48 -0300

    Ported to Python3, optimized and improved.

--- corrigelog.py
+++ corrigelog.py
@@ -1,26 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 from os import system
-system("touch convertido.log")
+system('touch converted.log')
 
-log=open("latest.log", "r")
-convertido=open("convertido.log", "w")
-for linea in log.readlines():
-	if linea[0] == "[":
-		hora = int(linea[1:3])
-		hora = hora - 5
-		hora = str(hora)
-		hora = hora.replace('-1','23')
-		hora = hora.replace('-2','22')
-		hora = hora.replace('-3','21')
-		hora = hora.replace('-4','20')
-		hora = hora.replace('-5','19')
-		if int(hora) < 10:
-			hora = '0' + hora
-		convertido.write("["+hora+linea[3:])
-	else:
-		convertido.write(linea)
-		continue
-log.close()
-convertido.close()
+hour_difference = 5
+
+with open('lastest.log', 'r') as log:
+    with open('converted.log', 'w') as converted:
+        for line in log.readlines():
+            if line[0] == '[':
+                hour = int(line[1:3]) - hour_difference
+
+                if int(hour) < 0: 
+                    hour += 24
+
+                hour = str(hour)
+
+                if len(hour) == 1:
+                    hour = '0' + hour
+
+                converted.write('[' + hour + line[3:])
+            else:
+                converted.write(line)
